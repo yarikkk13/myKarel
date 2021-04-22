a1e111ed1e84570a67324a87f615648f0b688dbd
yarikkk13 <yaroslav.mahas@gmail.com>
2021-04-22 17:14:47 +0300

    1 st commit

---
+++ src/com/shpp/p2p/cs/ymahas/myKarel2.java
@@ -0,0 +1,15 @@
+package com.shpp.p2p.cs.ymahas;
+
+import com.shpp.karel.KarelTheRobot;
+
+public class myKarel2 extends KarelTheRobot {
+
+
+
+    public void run() throws Exception {
+        turnLeft();
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
+}
