f1d97b0ac5ed9a5065866c2b033ffb3e99072e5f
yarikkk13 <yaroslav.mahas@gmail.com>
2021-04-23 16:16:09 +0300

    first is done with comments

--- src/com/shpp/p2p/cs/ymahas/Assignment1/Assignment1Part1.java
+++ src/com/shpp/p2p/cs/ymahas/Assignment1/Assignment1Part1.java
@@ -8,7 +8,7 @@ public class Assignment1Part1 extends KarelTheRobot {
         pickBeeperAndTurnAround();
         returnToTheStart();
     }
-
+// in this action we are going to newspaper
     private void goToTheNewspaper() throws Exception {
         turnRight();
         move();
@@ -18,19 +18,19 @@ public class Assignment1Part1 extends KarelTheRobot {
         move();
         move();
     }
-
+// we are heading 90 degrees of the start position
     private void turnRight() throws Exception {
         turnLeft();
         turnLeft();
         turnLeft();
     }
-
+// in this action we pick the beeper and heading the way we came out
     private void pickBeeperAndTurnAround() throws Exception {
         pickBeeper();
         turnLeft();
         turnLeft();
     }
-
+// in this action we go the point of start
     private void returnToTheStart() throws Exception {
         move();
         move();
