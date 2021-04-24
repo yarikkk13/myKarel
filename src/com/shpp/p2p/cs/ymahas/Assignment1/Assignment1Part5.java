ac84ec5cecbae9a618b005af4e1175f234c84615
yarikkk13 <yaroslav.mahas@gmail.com>
2021-04-24 17:20:47 +0300

    fourth task v.2

--- src/com/shpp/p2p/cs/ymahas/Assignment1/Assignment1Part5.java
+++ src/com/shpp/p2p/cs/ymahas/Assignment1/Assignment1Part5.java
@@ -2,7 +2,7 @@ package com.shpp.p2p.cs.ymahas.Assignment1;
 
 import com.shpp.karel.KarelTheRobot;
 
-public class Assignment1Part4 extends KarelTheRobot {
+public class Assignment1Part5 extends KarelTheRobot {
     public void run() throws Exception {
         putLikeAChessMaster();
     }
