35914b6db891c85c07d12e72e7b2734146e7c99d
yarikkk13 <yaroslav.mahas@gmail.com>
2021-04-24 14:32:47 +0300

    second solving of third task is done without comments

--- src/com/shpp/p2p/cs/ymahas/Assignment1/Assignment1Part3.java
+++ src/com/shpp/p2p/cs/ymahas/Assignment1/Assignment1Part3.java
@@ -3,35 +3,108 @@ package com.shpp.p2p.cs.ymahas.Assignment1;
 import com.shpp.karel.KarelTheRobot;
 
 
+//public class Assignment1Part3 extends KarelTheRobot {
+//    int i = 0;
+//
+//    public void run() throws Exception {
+//        findTheMidAndPutBeeper();
+//    }
+//
+//    // in this action we rotate our Karel on 180 degrees
+//    private void turnAround() throws Exception {
+//        turnLeft();
+//        turnLeft();
+//    }
+//    //in this action we explore the length of south row
+//    private void counter() throws Exception {
+//        while (frontIsClear()) {
+//            move();
+//            i++;
+//        }
+//    }
+//    // in this action we go the mid of the south row
+//    private void goToMid() throws Exception {
+//        for (int j = 0; j < i / 2; j++) {
+//            move();
+//        }
+//    }
+//    // in this action we do what i describe on previous comments, and then Karel puts beeper in mid
+//    private void findTheMidAndPutBeeper() throws Exception {
+//        counter();
+//        turnAround();
+//        goToMid();
+//        putBeeper();
+//    }
+//}
+
+
 public class Assignment1Part3 extends KarelTheRobot {
-    int i = 0;
 
     public void run() throws Exception {
         findTheMidAndPutBeeper();
     }
+
     // in this action we rotate our Karel on 180 degrees
     private void turnAround() throws Exception {
         turnLeft();
         turnLeft();
     }
-    //in this action we explore the length of south row
-    private void counter() throws Exception {
+
+    //in this action we put beepers on the whole length of south row
+    private void putBeepers() throws Exception {
+        putBeeper();
         while (frontIsClear()) {
             move();
-            i++;
+            putBeeper();
         }
     }
+
     // in this action we go the mid of the south row
-    private void goToMid() throws Exception {
-        for (int j = 0; j < i / 2; j++) {
+    private void pickingUp() throws Exception {
+        while (frontIsClear()) {
             move();
         }
+        pickBeeper();
+        turnAround();
+        while (frontIsClear()) {
+            move();
+        }
+        pickBeeper();
+        turnAround();
+        move();
     }
+
+    private void findingKriini() throws Exception {
+        while (beepersPresent()) {
+            move();
+        }
+        turnAround();
+        move();
+        pickBeeper();
+    }
+
+    private void theLastMove() throws Exception {
+        turnAround();
+        if (frontIsClear()) {
+            move();
+        }
+        putBeeper();
+    }
+
+
     // in this action we do what i describe on previous comments, and then Karel puts beeper in mid
     private void findTheMidAndPutBeeper() throws Exception {
-        counter();
-        turnAround();
-        goToMid();
-        putBeeper();
+        if (frontIsBlocked()) {
+            putBeeper();
+        } else {
+            putBeepers();
+            turnAround();
+            pickingUp();
+            while (beepersPresent()) {
+                findingKriini();
+                move();
+            }
+            theLastMove();
+        }
     }
 }
